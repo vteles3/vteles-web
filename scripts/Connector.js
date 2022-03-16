534cee634236903b6ecbc59cbfee49466b344cab
vteles-manager <duongpm13vhomes@gmail.com>
2022-03-16 14:23:44 +0700

    always ssl

--- scripts/Connector.js
+++ scripts/Connector.js
@@ -8,7 +8,7 @@ class Connector {
     config.host = serverAddress;
     config.port = serverPort;
     config.debug = false;
-    config.useSSL = serverPort == 8080 ? false : true;
+    config.useSSL = true;
   
     this.sfs = new SFS2X.SmartFox(config);
     
